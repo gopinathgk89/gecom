aaab02250f4ada4d15704a2b7fa77d4f8b91a42a
gopinathgk89 <gopinathgk44@gmail.com>
2026-03-26 11:56:23 -0700

    Update home.js

--- src/home.js
+++ src/home.js
@@ -1,30 +1,24 @@
 import './App.css';
-
 import 'bootstrap-icons/font/bootstrap-icons.css';
 import Navbar from './Navbar.js'; 
 import ImageSlider from "./slider.js"; 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import Bestproducts from './bestproducts.js';
-import Trendingproducts from "./trendingproducts.js";
 import Footer from "./footer.js"; 
 import Rewiews from "./clientrewiews.js";
-import React, { useState,useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
- 
-
-      
 
 function Home({ searchTerm, setSearchTerm }) {
 
-const [marqueeText, setMarqueeText] = useState('');
+  const [marqueeText, setMarqueeText] = useState('');
 
-
-useEffect(() => {
+  useEffect(() => {
     fetchMarquee();
   }, []);
 
-   const fetchMarquee = async () => {
+  const fetchMarquee = async () => {
     try {
       const res = await axios.get('http://localhost:8013/api/marquee/latest');
       if (res.data && res.data.content) {
@@ -34,35 +28,25 @@ useEffect(() => {
       console.error("Error fetching marquee text", error);
     }
   };
-   
-   return(
-  
-   <div >
 
-    
-    
+  return (
+    <div>
 
-      <div className="top-message" style={{height:'30px'}}>
-      <marquee direction="left" style={{position:'relative',bottom:'3px'}}>
+      {/* ✅ marquee replaced with modern animation */}
+      <div className="top-message">
+        <div className="marquee-text">
           {marqueeText}
-        </marquee>      
-      </div> 
+        </div>
+      </div>
+
       <Navbar setSearchTerm={setSearchTerm} />
       <ImageSlider/>
       <Bestproducts searchTerm={searchTerm}/>
-       
-    
       <Rewiews/>
       <Footer/>
 
-       
-     
-
-  </div>
-
-
-
-   	);
+    </div>
+  );
 }
-export default Home;
 
+export default Home;
